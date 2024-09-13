b337075f0900d9fcd913cc33064b51033fda345a
Vidar Ramdal <vramdal@gmail.com>
2024-09-13 18:03:06 +0200

    Web-visning

--- tests/Kode.test.js
+++ tests/Kode.test.js
@@ -26,3 +26,7 @@ describe('kode', () => {
     })
 });
 
+describe('web', () => {
+
+});
+
---
+++ web.js
@@ -0,0 +1,26 @@
+const rowClasses = ["pkt-txt-54", "pkt-txt-40", "pkt-txt-30", "pkt-txt-20", "pkt-txt-10"];
+
+const SHEET_NAME_DISPLAY_NAMES = "Visningsnavn";
+const slackNamesToDisplayNames = new Map(Object.entries(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME_DISPLAY_NAMES).getDataRange().getValues().slice(1)));
+
+const getDisplayName = (slackName) => {
+    return slackNamesToDisplayNames.has(slackName) ? slackNamesToDisplayNames.get(slackName) : slackName;
+}
+
+const getRowClass = (index) => {
+    return rowClasses[index > rowClasses.length - 1 ? rowClasses.length - 1 : index];
+}
+
+const doGet = (event) => {
+    const data = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Rankingtabell').getDataRange().getValues().slice(1).map(row => ({
+        navn: row[0],
+        poeng: row[1]
+    })).map((row, index) => ({
+        ...row,
+        displayName: getDisplayName(row.navn),
+        rowClass: getRowClass(index)
+    }));
+    var template = HtmlService.createTemplateFromFile('ranking');
+    template.data = data;
+    return template.evaluate();
+}
