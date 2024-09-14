7102b547bc4699765acd0a9d8a663ed3b6fdd9fa
Vidar Ramdal <vramdal@gmail.com>
2024-09-14 14:35:05 +0200

    Fikser visningsnavn, og autorefresh

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 const rowClasses = ["pkt-txt-54", "pkt-txt-40", "pkt-txt-30", "pkt-txt-20", "pkt-txt-10"];
 
 const SHEET_NAME_DISPLAY_NAMES = "Visningsnavn";
-const slackNamesToDisplayNames = new Map(Object.entries(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME_DISPLAY_NAMES).getDataRange().getValues().slice(1)));
+const visningsnavnValues = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME_DISPLAY_NAMES).getDataRange().getValues();
+const slackNamesToDisplayNames = new Map(visningsnavnValues.slice(1));
 
 const getDisplayName = (slackName) => {
     return slackNamesToDisplayNames.has(slackName) ? slackNamesToDisplayNames.get(slackName) : slackName;
@@ -11,8 +12,8 @@ const getRowClass = (index) => {
     return rowClasses[index > rowClasses.length - 1 ? rowClasses.length - 1 : index];
 }
 
-const doGet = (event) => {
-    const data = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Rankingtabell').getDataRange().getValues().slice(1).map(row => ({
+const getDataForWebDisplay = () => {
+    return SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Rankingtabell').getDataRange().getValues().slice(1).map(row => ({
         navn: row[0],
         poeng: row[1]
     })).map((row, index) => ({
@@ -20,6 +21,10 @@ const doGet = (event) => {
         displayName: getDisplayName(row.navn),
         rowClass: getRowClass(index)
     }));
+};
+
+const doGet = (event) => {
+    const data = getDataForWebDisplay();
     var template = HtmlService.createTemplateFromFile('ranking');
     template.data = data;
     return template.evaluate();
